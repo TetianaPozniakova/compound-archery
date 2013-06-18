9229d4f442d900fa48449c08be1a24ca02489a85
noctule <nightbat@gothic.com.ua>
2013-06-18 21:03:31 +0300

    Adding bootstrap.

---
+++ kat/kat_main_site/urls.py
@@ -0,0 +1,12 @@
+__author__ = 'noctule'
+from django.conf.urls import patterns
+from django.views.generic import TemplateView
+from kat_main_site.views import main_page
+
+
+urlpatterns = patterns('',
+    (r'^$', main_page),
+
+    # Static pages
+    (r'^contacts/', TemplateView.as_view(template_name="contacts_page.html")),
+)
