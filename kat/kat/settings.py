942e11997c59290b62b822a4e8216b82b940ff5f
noctule <nightbat@gothic.com.ua>
2013-09-18 07:03:20 +0300

    Alwaysdata settings.py

--- kat/kat/settings.py
+++ kat/kat/settings.py
@@ -15,18 +15,19 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'compound_archery',                      # Or path to database file if using sqlite3.
+        'NAME': 'noctule_compound_archery',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'compound_archery_postgre',
+        'USER': 'noctule_compound_archery',
         'PASSWORD': 'I2l0c1a3',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': 'postgresql1.alwaysdata.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+#ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*.alwaysdata.net', 'compound-archery.com.ua']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
