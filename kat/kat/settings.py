0eb862498c3a1bb378b2bda0f6a313b5c779b619
noctule <nightbat@gothic.com.ua>
2013-09-28 00:30:45 +0300

    Local database 2

--- kat/kat/settings.py
+++ kat/kat/settings.py
@@ -19,7 +19,7 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'compound_archery_postgre',
         'PASSWORD': 'I2l0c1a3',
-        'HOST': 'postgresql1.alwaysdata.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
