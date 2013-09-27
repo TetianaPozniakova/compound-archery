108473542e5df77e9a04f2ed30680f0d05b75c26
noctule <nightbat@gothic.com.ua>
2013-09-28 00:27:02 +0300

    Local database

--- kat/kat/settings.py
+++ kat/kat/settings.py
@@ -15,9 +15,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'noctule_compound_archery',                      # Or path to database file if using sqlite3.
+        'NAME': 'compound_archery',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'noctule_compound_archery',
+        'USER': 'compound_archery_postgre',
         'PASSWORD': 'I2l0c1a3',
         'HOST': 'postgresql1.alwaysdata.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
