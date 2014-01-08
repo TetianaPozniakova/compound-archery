956853dbe56620b8b38dc4a8de39511555ebe03e
noctule <nightbat@gothic.com.ua>
2014-01-08 20:51:11 +0200

    Change admin url

--- kat/kat/urls.py
+++ kat/kat/urls.py
@@ -35,7 +35,7 @@ urlpatterns = patterns('',
     #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+    (r'^arrows/', include(admin.site.urls)),
 
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
